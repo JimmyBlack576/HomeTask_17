cb47351d6f0cf7cab8591b65af1c8ede80a80b2c
jimmyblackwood576@outlook.com <jimmyblackwood576@outlook.com>
2022-06-06 21:33:18 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_17/HomeTask_17.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int symbol_num;
+    char symbol;
+
+    cout << "Введите символ: \n";
+    cin >> symbol;
+    symbol_num = symbol;
+    if ((symbol_num == 33) || (symbol_num >= 44) &&
+        (symbol_num <= 46) || (symbol_num == 58) ||
+        (symbol_num == 59) || (symbol_num == 63))
+    {
+        cout << "Вы ввели знак препинания \n";
+    }
+    else if (symbol_num >= 46 && symbol_num <= 57)
+    {
+        cout << "Вы ввели цифру \n";
+    }
+
+    else if ((symbol_num >= 65 && symbol_num <= 90) ||
+        (symbol_num >= 97 && symbol_num <= 122))
+    {
+        cout << "Вы ввели букву \n";
+    }
+    else 
+    {
+        cout << "Я не знаю такого символа!\n";
+    }
+    
+}
+
